18dfc32589e249608dca865bb29792728f9acc9f
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-08-16 10:09:51 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@ function remove() {
     bubble = document.getElementById("bubble");
     bubble.remove();
 }
+    document.addEventListener("DOMContentLoaded", function createBubble(){
+    const bubble = document.createElement("div");
+    bubble.id = "bubble";
+    bubble.className ="bubble-class";
+    document.body.appendChild(bubble);
+}, false)
+
+
