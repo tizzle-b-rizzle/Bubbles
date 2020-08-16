8da028a85e3f8a8cd0794d794d9028f73ad6ade5
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-08-16 13:31:03 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-function remove() {
-    bubble = document.getElementById("bubble");
-    bubble.remove();
-}
-    const jsBubble = document.createElement("div");
-    jsbBubble.id = "bubble";
-    jsBubble.className ="bubble-class";
-    
+jsBubble = document.createElement("div");
+jsBubble.id = "bubble";
+jsBubble.className ="bubble-class";  
+jsBubble.onclick = function removeBubble() { //this seems to only work when you do the onclick like this
+    document.getElementById("page-grid").removeChild(jsBubble);
+} ;
 function createBubble() {
-    document.getElementById("page-grid").appendChild(bubble);
+    document.getElementById("page-grid").appendChild(jsBubble);
 }
+
+
