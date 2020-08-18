07e5152b6d6965921e24190b2af9d1d6baeae1d1
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-08-18 19:02:32 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-jsBubble = document.createElement("div");
-jsBubble.id = "bubble";
-jsBubble.className ="bubble-class";  
-jsBubble.onclick = function removeBubble() { //this seems to only work when you do the onclick like this
+
+function createBubble() {
+    jsBubble = document.createElement("div"); //creates a div
+    jsBubble.id = "bubble"; //the div id
+    jsBubble.className ="bubble-class";  //gives the div a class to style the bubbles
+    document.getElementById("page-grid").appendChild(jsBubble); //adds the div into the DOM
+    var randomNumber = Math.floor(Math.random() * 20) + 30; //generates a random number between 20 and 50
+    jsBubble.style.gridRow = randomNumber; //determines the row of the bubble when it's generated
+    jsBubble.style.gridColumn = randomNumber; //determines the column of the bubble when it's generated
+    jsBubble.onclick = function removeBubble() { //this seems to only work when you do the onclick like this
     document.getElementById("page-grid").removeChild(jsBubble);
 } ;
-function createBubble() {
-    document.getElementById("page-grid").appendChild(jsBubble);
-}
 
+}
 
