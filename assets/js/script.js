97a86a6cd0d3ed8056a482fd7fb89aa5323c3a96
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-08-22 13:20:43 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
-
+var noOfBubbles = 0;
 function createBubble() {
+    noOfBubbles += 1;
     jsBubble = document.createElement("div"); //creates a div
-    jsBubble.id = "bubble"; //the div id
+    jsBubble.id = "bubble" + noOfBubbles; //the div id
     jsBubble.className ="bubble-class";  //gives the div a class to style the bubbles
     document.getElementById("page-grid").appendChild(jsBubble); //adds the div into the DOM
-    var randomNumber = Math.floor(Math.random() * 20) + 30; //generates a random number between 20 and 50
+    var randomNumber = Math.floor(Math.random() * 20) + 20; //generates a random number between WORK OUT
     jsBubble.style.gridRow = randomNumber; //determines the row of the bubble when it's generated
     jsBubble.style.gridColumn = randomNumber; //determines the column of the bubble when it's generated
-    jsBubble.onclick = function removeBubble() { //this seems to only work when you do the onclick like this
-    document.getElementById("bubble").remove(); //removes the bubble, but currently only in the order they were created
+    var element = document.getElementById("bubble" + noOfBubbles); //each bubble
+    element.onclick = function removeBubble() { //when a bubble is clicked, that pubble us removes
+        noOfBubbles -= 1;
+        this.remove()
+    }
 } ;
 
-}
 
